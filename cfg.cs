6dfbb99204b9bdd1a6d4b0c4efdb7c61b0a66e00
VTStudio <2366645562@qq.com>
2026-07-14 20:03:05 +0800

    0714 2

--- cfg.cs
+++ cfg.cs
@@ -1,9 +1,23 @@
 // cfg.cs
+using System;
+
 namespace VTStudioToolBox
 {
     public static class Cfg
     {
-        public const string AppVersion = "[Release] 1.1 (Build.2607141815)";
+        private const string AppVersionPrefix = "[Release] 1.1 (Build.";
+
+        public static string AppVersion
+        {
+            get
+            {
+                var now = TimeZoneInfo.ConvertTimeFromUtc(
+                    DateTime.UtcNow,
+                    TimeZoneInfo.FindSystemTimeZoneById("China Standard Time"));
+                return $"{AppVersionPrefix}{now:yyMMddHHmm})";
+            }
+        }
+
         public const string Website = "https://visualtechstudio.github.io/";
         public const string GithubRepo = "https://github.com/VisualTechStudio/VTStudioToolBox";
         public const string GPLV3 = "https://choosealicense.com/licenses/gpl-3.0/";
