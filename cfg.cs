11ed7c84262cd6a934058a1bae50b03303ddd164
VTStudio <2366645562@qq.com>
2026-09-08 19:31:33 +0800

    0908 Update

--- cfg.cs
+++ cfg.cs
@@ -5,9 +5,9 @@ namespace VTStudioToolBox
 {
     public static class Cfg
     {
-        public const string AppVersion = "[Release] 26.2.6 (Build.2608010830)";
+        public const string AppVersion = "[Release] 26.2.8 (Build.2608010830)";
 
-        public const string Website = "https://visualtechstudio.github.io/";
+        public const string Website = "https://vtstudio.cloud/";
         public const string GithubRepo = "https://github.com/VisualTechStudio/VTStudioToolBox";
         public const string GPLV3 = "https://choosealicense.com/licenses/gpl-3.0/";
     }
